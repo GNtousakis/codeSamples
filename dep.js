e91c76d3191e5d199a3e405531def7c3621102d9
GNtousakis <greco1996@gmail.com>
2021-01-04 10:16:05 -0500

    Fix lalal

--- dep.js
+++ dep.js
@@ -1,6 +1,7 @@
 const packageName = process.argv[2] ? process.argv[2] : "@andromeda/lya";
 const PackageDependents = require("package-dependents");
- 
+const nameSet = new Set();
+
 var counter = 0;
 
 // Get package dependents
@@ -8,7 +9,11 @@ const getDep = (name) => {
   PackageDependents(name).then(packages => {
       packages.forEach(c => {
         console.log(c.name);
-        counter++;
+        // If the name has not appear again increase the counter
+        if (!nameSet.has(c.name)){
+          nameSet.add(c.name);
+          counter++;
+        }
         getDep(c.name);
     })
   })
