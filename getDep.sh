a6b20fe4d5c1c783d56b342b86a41b7b9dfbe6dc
GNtousakis <greco1996@gmail.com>
2021-01-10 04:31:55 -0500

    lALALALA

---
+++ getDep.sh
@@ -0,0 +1,45 @@
+echo "uppercase" >> stats.txt
+node-dependents uppercase  >> stats.txt
+sleep 15
+echo "slug" >> stats.txt
+node-dependents slug  >> stats.txt
+sleep 15
+echo "camel-case" >> stats.txt
+node-dependents camel-case  >> stats.txt
+sleep 15
+echo "r-trim" >> stats.txt
+node-dependents r-trim  >> stats.txt
+echo "constant-case" >> stats.txt
+sleep 15
+node-dependents constant-case  >> stats.txt
+sleep 15
+echo "pascal-case" >> stats.txt
+node-dependents pascal-case  >> stats.txt
+sleep 15
+echo "sentence-case" >> stats.txt
+node-dependents sentence-case  >> stats.txt
+echo "repeat" >> stats.txt
+sleep 15
+node-dependents repeat  >> stats.txt
+echo "title-case" >> stats.txt
+sleep 15
+node-dependents title-case   >> stats.txt
+echo "dot-case" >> stats.txt
+sleep 15
+node-dependents dot-case  >> stats.txt
+echo "snake-case" >> stats.txt
+sleep 15
+node-dependents snake-case   >> stats.txt
+echo "no-case" >> stats.txt
+sleep 15
+node-dependents no-case  >> stats.txt
+echo "space-case" >> stats.txt
+sleep 15
+node-dependents space-case  >> stats.txt
+echo "sanitize" >> stats.txt
+sleep 15
+node-dependents sanitize  >> stats.txt
+echo "strip-ansi" >> stats.txt
+sleep 15
+node-dependents strip-ansi  >> stats.txt
+
