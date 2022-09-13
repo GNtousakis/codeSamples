fb86d86110e5c9a9d506b13cd43692a11240e742
Grigoris Ntousakis <48942855+GNtousakis@users.noreply.github.com>
2022-09-13 14:27:25 +0300

    Add python template

---
+++ pythonTests/python_template.py
@@ -0,0 +1,45 @@
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+var (
+	logPath string
+)
+
+func init() {
+	flag.StringVar(&logPath, "lp", "./client.log", "The path to the log file (default ./client.log)")
+	flag.Parse()
+
+	// open log file
+	logFile, err := os.OpenFile(args.logPath, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.SetFlags(log.Ldate | log.Lmicroseconds | log.LUTC)
+	log.SetOutput(logFile)
+
+	// setup signal catching
+	sigs := make(chan os.Signal, 1)
+	// catch all signals since not explicitly listing
+	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
+	// method invoked upon seeing signal
+	go func() {
+		s := <-sigs
+		log.Printf("RECEIVED SIGNAL: %s", s)
+		os.Exit(1)
+	}()
+}
+
+func main() {
+	
+}
